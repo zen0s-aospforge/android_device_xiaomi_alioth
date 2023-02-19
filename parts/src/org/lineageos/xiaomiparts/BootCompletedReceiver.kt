3778e694f70211203970f76f073b1e25e94eac91
Ghosuto <clash.raja10@gmail.com>
2023-02-19 06:54:43 +0100

    alioth: parts: Implement HBM, Auto HBM, and DC Dimming support
    Changes include:
    - Implement High Brightness Mode (HBM) and Auto HBM
    - Add 7-second delay before disabling HBM to prevent flicker in shadow transitions
    - Skip redundant backlight writes when disabling HBM
    - Refresh QS brightness slider when HBM is active
    - Disable HBM and deactivate QS tile when screen turns off
    - Introduce time-based HBM auto-disable preference
    - Prevent HBM from activating when DC Dimming is enabled
    - Add DC Dimming feature and integrate under Settings → Display
    - Add DC Dimming QS tile and fix related SEPolicy denials
    - Align DC Dimming with exposure dim layer
    - Set HBM permissions and disable thermal throttling
    - Enable use_resource_processor for all SystemUI dependencies
    - Rename package and add Java support in parts
    - Fix SEPolicy issues and syntax errors
    
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: EmanuelCN <emanuelgithub@gmail.com>
    Co-authored-by: Arian <arian.kulmer@web.de>
    Co-authored-by: zenzer0s <praveenzalaki200@gmail.com>
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: EmanuelCN <emanuelgithub@gmail.com>
    Signed-off-by: zenzer0s <praveenzalaki200@gmail.com>

--- parts/src/org/lineageos/xiaomiparts/BootCompletedReceiver.kt
+++ parts/src/org/lineageos/xiaomiparts/BootCompletedReceiver.kt
@@ -9,8 +9,12 @@ package org.lineageos.xiaomiparts
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.os.SystemProperties
 import android.util.Log
+import androidx.preference.PreferenceManager
 import org.lineageos.xiaomiparts.thermal.ThermalUtils
+import org.lineageos.xiaomiparts.utils.FileUtils
 
 /** Everything begins at boot. */
 class BootCompletedReceiver : BroadcastReceiver() {
@@ -21,9 +25,16 @@ class BootCompletedReceiver : BroadcastReceiver() {
 
         Log.i(TAG, "Boot completed, starting services")
         ThermalUtils.getInstance(context).startService()
+        FileUtils.enableService(context)
+
+        val sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context)
+        val dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false)
+        FileUtils.writeLine(DC_DIMMING_NODE, if (dcDimmingEnabled) "1" else "0")
     }
 
     companion object {
         private const val TAG = "XiaomiParts-BCR"
+        private const val DC_DIMMING_ENABLE_KEY = "dc_dimming_enable"
+        private const val DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/dimlayer_exposure"
     }
 }
--- parts/src/org/lineageos/xiaomiparts/utils/FileUtils.kt
+++ parts/src/org/lineageos/xiaomiparts/utils/FileUtils.kt
@@ -6,8 +6,14 @@
 
 package org.lineageos.xiaomiparts.utils
 
+import android.content.Context
+import android.content.Intent
+import android.os.UserHandle
 import android.util.Log
+import kotlin.jvm.JvmStatic
 import java.io.File
+import org.lineageos.xiaomiparts.hbm.AutoHBMService
+import org.lineageos.xiaomiparts.hbm.HBMFragment
 
 private const val TAG = "FileUtils"
 
@@ -77,3 +83,76 @@ fun rename(srcPath: String, dstPath: String): Boolean =
     runCatching { File(srcPath).renameTo(File(dstPath)) }
         .onFailure { e -> Log.w(TAG, "Failed to rename $srcPath to $dstPath", e) }
         .getOrDefault(false)
+
+fun getFileValueAsBoolean(filename: String, defValue: Boolean): Boolean {
+    val fileValue = readOneLine(filename)
+    return if (fileValue != null) {
+        fileValue != "0"
+    } else {
+        defValue
+    }
+}
+
+fun getFileValue(filename: String, defValue: String): String {
+    val fileValue = readOneLine(filename)
+    return fileValue ?: defValue
+}
+
+private var mServiceEnabled = false
+
+private fun startService(context: Context) {
+    context.startServiceAsUser(Intent(context, AutoHBMService::class.java), UserHandle.CURRENT)
+    mServiceEnabled = true
+}
+
+private fun stopService(context: Context) {
+    mServiceEnabled = false
+    context.stopServiceAsUser(Intent(context, AutoHBMService::class.java), UserHandle.CURRENT)
+}
+
+fun enableService(context: Context) {
+    if (HBMFragment.isAUTOHBMEnabled(context) && !mServiceEnabled) {
+        startService(context)
+    } else if (!HBMFragment.isAUTOHBMEnabled(context) && mServiceEnabled) {
+        stopService(context)
+    }
+}
+
+/**
+ * Backwards-compatible facade for Java/Kotlin call sites that expect a `FileUtils` class/object.
+ * Some code imports `org.lineageos.xiaomiparts.utils.FileUtils` and calls methods on it. To avoid
+ * changing all call sites, expose an object with the small set of functions used elsewhere.
+ */
+object FileUtils {
+    @JvmStatic
+    fun enableService(context: Context) = org.lineageos.xiaomiparts.utils.enableService(context)
+
+    @JvmStatic
+    fun readOneLine(fileName: String): String? = org.lineageos.xiaomiparts.utils.readOneLine(fileName)
+
+    @JvmStatic
+    fun writeLine(fileName: String, value: String): Boolean = org.lineageos.xiaomiparts.utils.writeLine(fileName, value)
+
+    @JvmStatic
+    fun fileExists(fileName: String): Boolean = org.lineageos.xiaomiparts.utils.fileExists(fileName)
+
+    @JvmStatic
+    fun isFileReadable(fileName: String): Boolean = org.lineageos.xiaomiparts.utils.isFileReadable(fileName)
+
+    @JvmStatic
+    fun isFileWritable(fileName: String): Boolean = org.lineageos.xiaomiparts.utils.isFileWritable(fileName)
+
+    @JvmStatic
+    fun delete(fileName: String): Boolean = org.lineageos.xiaomiparts.utils.delete(fileName)
+
+    @JvmStatic
+    fun rename(srcPath: String, dstPath: String): Boolean = org.lineageos.xiaomiparts.utils.rename(srcPath, dstPath)
+
+    @JvmStatic
+    fun getFileValueAsBoolean(filename: String, defValue: Boolean): Boolean =
+        org.lineageos.xiaomiparts.utils.getFileValueAsBoolean(filename, defValue)
+
+    @JvmStatic
+    fun getFileValue(filename: String, defValue: String): String =
+        org.lineageos.xiaomiparts.utils.getFileValue(filename, defValue)
+}
