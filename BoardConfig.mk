19d2f508d21c787724357c3be660541496cdc794
Omkar Chandorkar <gotenksIN@aospa.co>
2024-01-09 18:38:55 +0900

    alioth: Use AOSP assert seperator
    - why deviate?
    
    Change-Id: I07a016d4d793c855ccbfac3c5251c22efdeb0a68
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,7 +88,7 @@ TARGET_KERNEL_CONFIG := vendor/alioth_defconfig
 
 
 # OTA assert
-TARGET_OTA_ASSERT_DEVICE := alioth,aliothin
+TARGET_OTA_ASSERT_DEVICE := alioth|aliothin
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
