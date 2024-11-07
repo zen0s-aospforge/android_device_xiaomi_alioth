6857004759e472dbd501ccad1d34c242d11674f7
peternmuller <pm563838@gmail.com>
2024-11-07 19:32:02 +0100

    alioth: parts: Introduce XiaomiParts * Checkout Lineage FileUtils and thermal service from https://github.com/pa-gr/android_device_xiaomi_sm8450-common/commits/vauxite/parts
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- device.mk
+++ device.mk
@@ -199,6 +199,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.composer-service
