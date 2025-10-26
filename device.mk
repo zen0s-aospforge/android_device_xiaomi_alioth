dffb208580c88b21bbc45ec96cb2685f91748ee5
zenzer0s <praveenzalaki200@gmail.com>
2025-10-26 14:20:54 +0530

    alioth: add support for ViPER4Android FX
    - Use TARGET_INCLUDE_VIPERFX := true in lineage_$device.mk

--- device.mk
+++ device.mk
@@ -41,6 +41,9 @@ $(call inherit-product, vendor/xiaomi/alioth/alioth-vendor.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# ViPER4Android FX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
