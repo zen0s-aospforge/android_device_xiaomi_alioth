edde0fa1f2a0afd8cbf66ab887515a926b0ef1b2
zenzer0s <praveenzalaki200@gmail.com>
2025-10-27 23:59:47 +0530

    alioth: decommonize sm8250-common
    - Merged device specific tree [ device_xiaomi_alioth https://github.com/xiaomi-sm8250-devs/android_device_xiaomi_alioth.git ] into sm8250-common
    - dropped tablet specific stuffs
    - dropped xiaomi parts package
    - dropped udfps support
    - rework, reorgranize and cleanup configs and overlays

--- extract-files.py
+++ extract-files.py
@@ -10,6 +10,7 @@ from extract_utils.fixups_blob import (
 )
 from extract_utils.fixups_lib import (
     lib_fixups,
+    lib_fixups_user_type,
 )
 from extract_utils.main import (
     ExtractUtils,
@@ -58,11 +59,48 @@ blob_fixups: blob_fixups_user_type = {
         .clear_symbol_version('AHardwareBuffer_unlock'),
     'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
         .binary_regex_replace(b'\x9A\x0A\x00\x94', b'\x1F\x20\x03\xD5'),
+    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libbinder_shim.so')
+        .add_needed('libinput_shim.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
+    'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
+        .regex_replace('.*seclabel u:r:mi_thermald:s0\n', ''),
+    'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
+        .add_line_if_missing('gettid: 1'),
+    'vendor/lib64/libril-qc-hal-qmi.so': blob_fixup()
+        .binary_regex_replace(b'ro.product.vendor.device', b'ro.vendor.radio.midevice'),
+    'vendor/lib64/libwvhidl.so': blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+    'vendor/lib64/mediadrm/libwvdrmengine.so': blob_fixup()
+        .add_needed('libcrypto_shim.so'),
 }  # fmt: skip
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+    ): lib_fixup_vendor_suffix,
+}
 
 namespace_imports = [
     'hardware/qcom-caf/common/libqti-perfd-client',
     'hardware/qcom-caf/sm8250',
+    'hardware/qcom-caf/wlan',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/qcom/opensource/commonsys/display',
+    'vendor/qcom/opensource/dataservices',
     'vendor/qcom/opensource/display',
     'vendor/xiaomi/sm8250-common',
 ]
@@ -75,6 +113,8 @@ module = ExtractUtilsModule(
     namespace_imports=namespace_imports,
 )
 
+module.add_proprietary_file('proprietary-files-phone.txt')
+
 if __name__ == '__main__':
-    utils = ExtractUtils.device_with_common(module, 'sm8250-common', module.vendor)
+    utils = ExtractUtils.device(module)
     utils.run()
