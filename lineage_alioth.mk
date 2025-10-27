414e2945804331eaee970301ce675d9061528c28
zenzer0s <praveenzalaki200@gmail.com>
2025-10-28 01:45:24 +0530

    alioth: Bringup AxionOS

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -33,3 +33,41 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=POCO/alioth_global/alioth:13/TKQ1.221114.001/V816.0.3.0.TKHMIXM:user/release-keys \
     DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
+
+#AxionAosp Flags
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_DISABLE_EPPE := true
+TARGET_ENABLE_BLUR := true
+
+AXION_MAINTAINER := zenzer0s
+
+AXION_PROCESSOR := Snapdragon®_870
+
+AXION_CAMERA_REAR_INFO := 48,8,5 
+AXION_CAMERA_FRONT_INFO := 20
+
+#ViperFX
+TARGET_INCLUDE_VIPERFX := true
+
+#cpu
+AXION_CPU_SMALL_CORES := 0,1,2,3
+AXION_CPU_BIG_CORES := 4,5,6,7
+
+# Dex2oat opti
+DEX2OAT_CORES := 0,1
+DEX2OAT_THREADS := 2
+
+# Flash Strength 
+TORCH_STR_SUPPORTED := true
+
+# Charging
+BYPASS_CHARGE_SUPPORTED ?= true
+
+# CPU governor support
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := schedutil
+PERF_ANIM_OVERRIDE := true
+
+GPU_FREQS_PATH := /sys/class/kgsl/kgsl-3d0/devfreq/available_frequencies
+GPU_MIN_FREQ_PATH := /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
