e551309cab9b976fd2b2aba509280040752ea5c9
zenzer0s <praveenzalaki200@gmail.com>
2025-11-06 16:47:57 +0530

    alioth: overlay: Add hwc conditionals to POCO and Mi11X overlays

--- device.mk
+++ device.mk
@@ -343,7 +343,9 @@ PRODUCT_PACKAGES += \
     NfcOverlayAlioth \
     SecureElementOverlayAlioth \
     SettingsOverlayAlioth \
-    SettingsProviderOverlayAlioth \
+    SettingsProviderOverlayAliothPOCO \
+    SettingsProviderOverlayAliothRedmi \
+    SettingsProviderOverlayAliothMi11X \
     SystemUIOverlayAlioth \
     TelephonyOverlayAlioth \
     WifiOverlayAlioth
