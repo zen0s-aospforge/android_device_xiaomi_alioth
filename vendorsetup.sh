fed94cd5b2f4599a25d7ef7eae0fdb8f978eb9ee
zenzer0s <praveenzalaki200@gmail.com>
2025-11-20 19:03:08 +0530

    alioth: add ksu nextpatch.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 base64 -d device/xiaomi/alioth/configs/camera/secret > device/xiaomi/alioth/configs/camera/st_license.lic
+
+cd kernel/xiaomi/sm8250
+chmod +x nextpatch.sh && bash nextpatch.sh
+rm -rf KernelSU-Next/userspace/su
+cd ../../..
